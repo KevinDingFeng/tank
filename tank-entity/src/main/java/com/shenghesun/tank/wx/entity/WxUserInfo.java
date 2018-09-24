21fe55f5380b1d58a7c7903f872a179bc9733577
Kevin <chengrq@shenghesun.com>
2018-09-25 06:42:16 +0800

    kevin 普通用戶信息添加 聯係方式字段

--- tank-entity/src/main/java/com/shenghesun/tank/wx/entity/WxUserInfo.java
+++ tank-entity/src/main/java/com/shenghesun/tank/wx/entity/WxUserInfo.java
@@ -58,6 +58,15 @@ public class WxUserInfo extends BaseEntity {
 	private String language;
 	@Column(nullable = true, length = 255)
 	private String privilege;
+	/**
+	 * 联系方式
+	 */
+	@Column(nullable = true, length = 32)
+	private String cellphone;
+	/**
+	 * 联系方式是否通过了校验
+	 */
+	private boolean cellphoneVerified = false;
 	
 	/**
 	 * 是否已删除
